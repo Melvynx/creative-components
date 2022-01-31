9c9e0f0855b04bb678db27dccd3073b16f3a9e47
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-01-31 17:25:29 +0100

    fix document warning

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,9 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <title>React component</title>
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
