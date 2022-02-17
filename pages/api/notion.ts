0243f77e1695a8d79c068035a47e6f0892b6ea3e
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-02-17 20:50:53 +0100

    split notion api and add patch request

--- pages/api/notion.ts
+++ pages/api/notion.ts
@@ -1,10 +1,33 @@
 import { Client } from "@notionhq/client";
 import { ApiHandler } from "~/interfaces/next-api";
 
-const client = new Client({ auth: process.env.NOTION_API_TOKEN });
+const notion = new Client({ auth: process.env.NOTION_API_TOKEN });
 
 const handler: ApiHandler = async (req, res) => {
-  const response = await client.databases.query({
+  if (req.method === "PATCH") {
+    void patchHandler(req, res);
+  }
+  if (req.method === "GET") {
+    void getHandler(req, res);
+  }
+  res.status(404);
+};
+
+const patchHandler: ApiHandler = async (req, res) => {
+  const response = await notion.pages.update({
+    page_id: req.body.pageId,
+    properties: {
+      Read: {
+        checkbox: true,
+      },
+    },
+  });
+  res.status(200).json(response);
+  return;
+};
+
+const getHandler: ApiHandler = async (req, res) => {
+  const response = await notion.databases.query({
     database_id: "5b0fd593d0b74fa6a019c039ee362344",
     filter: {
       and: [
